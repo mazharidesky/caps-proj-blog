53dbbc567ddbd86d3446eea4ae9fb0ef31bbfa4e
mazharidesky <muhammad.210170230@mhs.unimal.ac.id>
2024-10-29 23:56:50 +0700

    add: folder routes

--- routes/app.js
+++ routes/app.js
@@ -4,6 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Our first express app");
+});
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
