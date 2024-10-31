113b942e80b805140e5d2a668c18f33ffb86dce3
mazharidesky <muhammad.210170230@mhs.unimal.ac.id>
2024-11-01 03:42:14 +0700

    feat: showing data on the top form

--- routes/app.js
+++ routes/app.js
@@ -4,10 +4,15 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", { title: req.body.title, content: req.body.content });
 });
 
 app.listen(PORT, () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./public/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1240px",
+          "2xl": "1400px",
+        },
+      },
+    },
   },
   plugins: [],
 };
